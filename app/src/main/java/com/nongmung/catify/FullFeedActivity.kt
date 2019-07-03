f3678581c5ec33353c7107b19d621849c7109b3f
Unknown <tongnop2540@gmail.com>
2019-07-03 11:00:32 +0700

    add post cat

---
+++ app/src/main/java/com/nongmung/catify/FullFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.nongmung.catify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FullFeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_feed)
+    }
+}
